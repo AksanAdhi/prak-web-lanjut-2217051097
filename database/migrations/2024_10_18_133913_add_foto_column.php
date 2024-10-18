76c8382d2fffed0911ffe62cb469ba6abd5381a4
AksanAdhi <aksanadhipratama.19@gmail.com>
2024-10-18 22:16:06 +0700

    modul 6

--- database/migrations/2024_10_18_133913_add_foto_column.php
+++ database/migrations/2024_10_18_133913_add_foto_column.php
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('user',function (Blueprint $table){
+            $table->string('foto')->nullable();
+            
+        });
     }
 
     /**
@@ -19,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('user', function(Blueprint $table){
+            $table->dropColumn('foto');
+        });
     }
 };
--- resources/views/list_user.blade.php
+++ resources/views/list_user.blade.php
@@ -32,7 +32,18 @@
                 </td>
                 <td>
                     <!-- View -->
-                    <a href="{{ route('user.show', $user->id') }}" class="btn btn-warning mb-3">Detail</a>
+                    <a href="{{ route('user.show', $user['id']) }}" class="btn btn-primary btn-sm">View</a>
+
+                    <!-- Edit -->
+                    <a href="{{ route('user.edit', $user['id']) }}" class="btn btn-warning btn-sm">Edit</a>
+
+                    <!-- Delete -->
+                    <form action="{{ route('user.destroy', $user['id']) }}" method="POST" style="display:inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm"
+                            onclick="return confirm('Apakah Anda yakin ingin menghapus user ini?')">Delete</button>
+                    </form>
                 </td>
             </tr>
             <?php
